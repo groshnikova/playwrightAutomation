cf48f7f37692ea042c8e20f2e21a6ac86aa41925
Karina Groshnikova <kgroshnikova@gmail.com>
2025-06-16 23:24:07 -0400

    changed headed mode to headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'https://www.lambdatest.com',
     baseURL: 'https://demoqa.com',
-    headless: false,
+    headless: true,
     //global setup only
     //storageState: './.auth/user.json', // Path to the authentication state file
     // Slow down by 50ms for each action. Useful to watch the actions. */
